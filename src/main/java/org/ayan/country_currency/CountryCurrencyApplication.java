07bd5c2e9919e61a95c7dfcdbd9c4794eeef05d9
Ayan Sengupta <as6485@gmail.com>
2025-08-03 23:50:20 +0530

    First commit

---
+++ src/main/java/org/ayan/country_currency/CountryCurrencyApplication.java
@@ -0,0 +1,13 @@
+package org.ayan.country_currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ayan/country_currency/CountryCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ayan.country_currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
